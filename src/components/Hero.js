5380a456b531894a7bf1c6692e7511ba78e857c0
amirkhalifa285 <amir.kh28@hotmail.com>
2024-09-18 15:26:46 +0300

    fixing build error

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import styled, { useTheme } from 'styled-components';
+/* import styled, { useTheme } from 'styled-components'; */
 import { motion } from 'framer-motion';
 
 const HeroSection = styled.section`
