ba92be2ff74d86153a24ab14475194fed7f11579
amirkhalifa285 <amir.kh28@hotmail.com>
2025-01-09 22:35:49 +0200

    remove unwanted import

--- src/App.js
+++ src/App.js
@@ -11,7 +11,6 @@ import Projects from './components/Projects';
 import Skills from './components/Skills';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
-import styled from 'styled-components';
 
 
 function App() {
